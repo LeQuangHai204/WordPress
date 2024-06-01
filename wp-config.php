da8008e2a90d2f135bbbd76329c97b9b014210d3
lequanghai <lequanghai.private@gmail.com>
2024-06-01 13:23:19 +0700

    Modify config.php for external DB

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'test' );
+define( 'DB_NAME', 'word_press' );
 
 /** Database username */
 define( 'DB_USER', 'admin' );
