4e352b29682215b59842f3671c5582480e4d07c6
lequanghai <lequanghai.private@gmail.com>
2024-06-01 14:02:57 +0700

    ds

--- scripts/helper.sh
+++ scripts/helper.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# sudo dnf install -y git
-# cd /tmp/
-# git clone https://github.com/LeQuangHai204/WordPress.git
-# cd WordPress/
+sudo dnf install -y git
+cd /tmp/
+git clone https://github.com/LeQuangHai204/WordPress.git
+cd WordPress/
 
-# sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
-# sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y
-# sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
-# sudo dnf install mysql-community-client -y
-# sudo rm mysql80-community-release-el9-1.noarch.rpm
+sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
+sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
+sudo dnf install mysql-community-client -y
+sudo rm mysql80-community-release-el9-1.noarch.rpm
 
 aws deploy delete-application --application-name WordPress_App
 aws deploy create-application --application-name WordPress_App
