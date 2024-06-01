b527e6b536a93f78a224ee7eee44a1fa891a1736
lequanghai <lequanghai.private@gmail.com>
2024-06-01 14:53:56 +0700

    fds

--- scripts/create_test_db.sh
+++ scripts/create_test_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo mysql -u root <<CREATE_TEST_DB
+sudo mysql -u admin -p Hai204fromtheocean -h cos40006-codedeploy-rds.cwgruczxadko.us-east-1.rds.amazonaws.com -P 3306<<CREATE_TEST_DB
 CREATE DATABASE IF NOT EXISTS test;
 CREATE_TEST_DB
