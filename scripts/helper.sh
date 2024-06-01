022b27ea642ba451c6acf084962725a4e4f6fb11
lequanghai <lequanghai.private@gmail.com>
2024-06-01 14:15:31 +0700

    ds

--- scripts/helper.sh
+++ scripts/helper.sh
@@ -5,11 +5,7 @@ cd /tmp/
 git clone https://github.com/LeQuangHai204/WordPress.git
 cd WordPress/
 
-sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
-sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y
-sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
-sudo dnf install mysql-community-client -y
-sudo rm mysql80-community-release-el9-1.noarch.rpm
+
 
 aws deploy delete-application --application-name WordPress_App
 aws deploy create-application --application-name WordPress_App
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sudo dnf install php php-cli php-common php-mysqlnd php-json php-xml -y
 # sudo dnf install mariadb105-server -y
+sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
+sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
+sudo dnf install mysql-community-client -y
+sudo rm mysql80-community-release-el9-1.noarch.rpm
