26cd0b4a59532f9d936b01c16237bb2bd00db4a0
lequanghai <lequanghai.private@gmail.com>
2024-06-01 15:10:33 +0700

    ds

--- scripts/create_test_db.sh
+++ scripts/create_test_db.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo mysql -u admin -p Hai204fromtheocean -h cos40006-codedeploy-rds.cwgruczxadko.us-east-1.rds.amazonaws.com -P 3306<<CREATE_TEST_DB
+
+# Connect to the MySQL RDS instance and create a new database if it doesn't already exist
+mysql -u admin -pHai204fromtheocean -h cos40006-codedeploy-rds.cwgruczxadko.us-east-1.rds.amazonaws.com -P 3306 <<CREATE_TEST_DB
 CREATE DATABASE IF NOT EXISTS test;
 CREATE_TEST_DB
